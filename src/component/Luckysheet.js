02d44c68141da394a200c395218171801f4fe80f
aaronamm <theeranitp@gmail.com>
2020-10-06 00:38:54 +0700

    Convert Luckysheet to a function component

--- src/component/Luckysheet.js
+++ src/component/Luckysheet.js
@@ -1,29 +1,26 @@
-import React from 'react';
-class Luckysheet extends React.Component {
+import React, { useEffect } from 'react';
 
-    componentDidMount() {
-        const luckysheet = window.luckysheet;
-        luckysheet.create({
-            container: "luckysheet",
-        });
-    }
-    render() {
-        const luckyCss = {
-            margin: '0px',
-            padding: '0px',
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            left: '0px',
-            top: '0px'
-        }
-        return (
-            <div
-            id="luckysheet"
-            style={luckyCss}
-            ></div>
-        )
-    }
+const Luckysheet = () => {
+  useEffect(() => {
+    const luckysheet = window.luckysheet;
+    luckysheet.create({
+      container: 'luckysheet',
+    });
+  }, []);
+
+  const luckyCss = {
+    margin: '0px',
+    padding: '0px',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    left: '0px',
+    top: '0px'
+  };
+
+  return (
+    <div id="luckysheet" style={luckyCss}></div>
+  );
 }
 
-export default Luckysheet
+export default Luckysheet;
